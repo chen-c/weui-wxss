cbd9a79272f6577be7856aa1ba7ffeb9d82236dd
Chen Chen <chenchen08@gmail.com>
2017-04-21 14:49:41 +0800

    尝试修改

--- dist/example/navbar/navbar.js
+++ dist/example/navbar/navbar.js
@@ -2,7 +2,7 @@ var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 
 Page({
     data: {
-        tabs: ["选项一", "选项二", "选项三"],
+        tabs: ["网点查询", "选项二", "选项三"],
         activeIndex: 1,
         sliderOffset: 0,
         sliderLeft: 0
